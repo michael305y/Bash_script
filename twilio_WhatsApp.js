346775bbfbb5e5b7c495b3ec92cf99688cbea710
Michael305y <antolando231@gmail.com>
2023-10-27 10:55:43 +0300

    cheki

--- twilio_WhatsApp.js
+++ twilio_WhatsApp.js
@@ -3,15 +3,16 @@
  * 
  */
 function send_Whatsapp() {
-    const accountSid = 'ACda659326823f452068d1b8c9114c4116';
-    const authToken = '635865545ce82838996c66210f9724ce';
+    const accountSid = 'YOUR_ACCOUNT_SID';
+    const authToken = 'YOUR_AUTH_TOKEN';
     const client = require('twilio')(accountSid, authToken);
+
 
     client.messages
         .create({
-            from: 'whatsapp:+14155238886',   // your twilio whatsapp number
+            from: 'whatsapp:TWILIO_WHATSAPP_NUMBER',   // your twilio whatsapp number
             body: 'Hello there! am twilio just testing whatsapp functionality cheers',
-            to: 'whatsapp:+254753761880'
+            to: 'whatsapp:YOUR_WHATSAPP_NUMBER'
         })
         .then(message => console.log(message.sid));
 }
--- twilio_demo_1.js
+++ twilio_demo_1.js
@@ -1,41 +1,18 @@
-// const accountSid = 'ACda659326823f452068d1b8c9114c4116';
-// const authToken = '635865545ce82838996c66210f9724ce';
-// // const client = require('twilio')(accountSid, authToken);
-
-// // client.messages
-// //     .create({
-// //         to: '+254753761880'
-// //     })
-// //     .then(message => console.log(message.sid))
-// //     .done();
-
-// // const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// // const authToken = process.env.TWILIO_AUTH_TOKEN;
-// const client = require('twilio')(accountSid, authToken);
-
-// client.messages
-//     .create({
-//         body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-//         from: '+18588599038',
-//         to: '+254753761880'
-//     })
-//     .then(message => console.log(message.sid));
-
-
-
-///////////// APPSCRIPT
 function send_SMS() {
-    const accountSid = 'ACda659326823f452068d1b8c9114c4116';
-    const authToken = '635865545ce82838996c66210f9724ce';
+    const accountSid = 'YOUR_ACCOUNT_SID';
+    const authToken = 'YOUR_AUTH_TOKEN';
     const client = require('twilio')(accountSid, authToken);
 
     client.messages
         .create({
             body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-            from: '+18588599038',
-            to: '+254753761880'
+            from: 'TWILIO_NUMBER',
+            to: 'YOUR_PHONE_NUMBER'
         })
         .then(message => console.log(message.sid));
 }
 
 send_SMS()
+
+
+
